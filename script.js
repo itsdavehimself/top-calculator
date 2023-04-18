77106a2034a2b00fa7eda678a793148f7c42dcdf
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-04-18 07:54:26 +0200

    Create button functions to display number pressed

--- script.js
+++ script.js
@@ -23,4 +23,55 @@ function operate(firstNum, operator, secondNum) {
     
 }
 
-operate(3, "*", 6)
+const displayBox = document.querySelector("output");
+
+const btnZero = document.querySelector(".zero")
+const btnOne = document.querySelector(".one")
+const btnTwo = document.querySelector(".two")
+const btnThree = document.querySelector(".three")
+const btnFour = document.querySelector(".four")
+const btnFive = document.querySelector(".five")
+const btnSix = document.querySelector(".six")
+const btnSeven = document.querySelector(".seven")
+const btnEight = document.querySelector(".eight")
+const btnNine = document.querySelector(".nine")
+
+btnZero.addEventListener("click", () => {
+    document.getElementById("output").value = "0";
+});
+
+btnOne.addEventListener("click", () => {
+    document.getElementById("output").value = "1";
+});
+
+btnTwo.addEventListener("click", () => {
+    document.getElementById("output").value = "2";
+});
+
+btnThree.addEventListener("click", () => {
+    document.getElementById("output").value = "3";
+});
+
+btnFour.addEventListener("click", () => {
+    document.getElementById("output").value = "4";
+});
+
+btnFive.addEventListener("click", () => {
+    document.getElementById("output").value = "5";
+});
+
+btnSix.addEventListener("click", () => {
+    document.getElementById("output").value = "6";
+});
+
+btnSeven.addEventListener("click", () => {
+    document.getElementById("output").value = "7";
+});
+
+btnEight.addEventListener("click", () => {
+    document.getElementById("output").value = "8";
+});
+
+btnNine.addEventListener("click", () => {
+    document.getElementById("output").value = "9";
+});
